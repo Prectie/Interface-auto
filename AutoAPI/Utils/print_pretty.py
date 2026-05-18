b64f7766f82f0e7f2cff9f6d9cd2732211012cd9
»ÆÌìÓî <820733450@qq.com>
2026-05-18 20:04:27 +0800

    重构命令行

--- AutoAPI/Utils/print_pretty.py
+++
@@ -1,9 +0,0 @@
-from rich import print as rprint
-from rich.pretty import Pretty
-
-
-def print_rich(data):
-    """
-      功能说明:使用 rich 美化打印 python 对象,适合调试复杂嵌套结构
-    """
-    rprint(Pretty(data, expand_all=True))
--- AutoAPI/练习.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(prog="AutoTest")
-
-parser.add_argument("--report-dir", dest="report_dirrrr")
-
-args = parser.parse_args()
-
-print(args.report_dirrrr)
